b6be2030c5a1e45a6d594b4fa22090df9496d63b
Mkassabeh <marwane_kassabeh@outlook.fr>
2023-08-09 14:03:43 +0200

    Initialization of the project rock-paper-scissors

---
+++ script.js
@@ -0,0 +1,12 @@
+function getComputerChoice(){
+    
+    let choice = ['rock','paper','scissors'];
+
+    let computerChoice = choice[Math.floor(Math.random() * choice.length)] ;
+
+    console.log(computerChoice)
+
+};
+
+
+getComputerChoice();
